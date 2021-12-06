a20bdc63cdecd9a40bf81a2ff0e9da981fd105fc
mugabe richard <mugabe.richard.37@gmail.com>
2021-12-06 11:47:34 +0200

    setting out the Routing and application config

---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import IPage from "../interface/page";
+import logging from "../config/logging";
+
+
+const HomePage: React.FunctionComponent<IPage> = props => {
+
+    return <p>Welcome Home Richard</p>
+}
