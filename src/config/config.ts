d79459b86965fd6de63c7eb4c3a0cecc2b522508
mugabe richard <mugabe.richard.37@gmail.com>
2021-12-06 18:26:29 +0200

    Connecting the application to firebase

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,11 @@
 const config = {
     firebase: {
+            apiKey: "AIzaSyDX-G6MIfAzc7SCMWxNbv2pUIDGBn8-CqY",
+            authDomain: "movieapp-dad47.firebaseapp.com",
+            projectId: "movieapp-dad47",
+            storageBucket: "movieapp-dad47.appspot.com",
+            messagingSenderId: "653971737876",
+            appId: "1:653971737876:web:bd620a025ab5ee09710601"
     }
 }
 
