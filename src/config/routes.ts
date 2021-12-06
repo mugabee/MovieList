3145d944bd2a15332aed5da3d30fba040e0c94e6
mugabe richard <mugabe.richard.37@gmail.com>
2021-12-06 19:09:38 +0200

    Connecting the application to firebase

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,33 +1,62 @@
-const DEFAULT_NAMESPACE = 'Client';
+import IRoute from "../interface/route";
+import ChangePasswordPage from "../pages/auth/change";
+import ForgotPasswordPage from "../pages/auth/forgot";
+import LoginPage from "../pages/auth/login";
+import LogoutPage from "../pages/auth/logout";
+import RegisterPage from "../pages/auth/register";
+import ResetPasswordPage from "../pages/auth/reset";
+import HomePage from "../pages/home";
 
-const info = (message: any, namespace?: string) => {
-    if (typeof message === 'string') {
-        console.log(`[${getDate()}] [${namespace || DEFAULT_NAMESPACE}] [INFO] ${message}`);
-    } else {
-        console.log(`[${getDate()}] [${namespace || DEFAULT_NAMESPACE}] [INFO]`, message);
+const routes: IRoute[] = [
+    {
+        path: '/',
+        exact: true,
+        component: HomePage,
+        name: 'Home Page',
+        protected: true
+    },
+    {
+        path: '/register',
+        exact: true,
+        component: RegisterPage,
+        name: 'Register Page',
+        protected: false
+    },
+    {
+        path: '/login',
+        exact: true,
+        component: LoginPage,
+        name: 'Login Page',
+        protected: false
+    },
+    {
+        path: '/change',
+        exact: true,
+        component: ChangePasswordPage,
+        name: 'Change Password Page',
+        protected: true
+    },
+    {
+        path: '/logout',
+        exact: true,
+        component: LogoutPage,
+        name: 'Logout Page',
+        protected: true
+    },
+    {
+        path: '/forget',
+        exact: true,
+        component: ForgotPasswordPage,
+        name: 'Forgot Password Page',
+        protected: false
+    },
+    {
+        path: '/reset',
+        exact: true,
+        component: ResetPasswordPage,
+        name: 'Reset Password Page',
+        protected: false
     }
-};
+];
 
-const warn = (message: any, namespace?: string) => {
-    if (typeof message === 'string') {
-        console.log(`[${getDate()}] [${namespace || DEFAULT_NAMESPACE}] [WARN] ${message}`);
-    } else {
-        console.log(`[${getDate()}] [${namespace || DEFAULT_NAMESPACE}] [WARN]`, message);
-    }
-};
-
-const error = (message: any, namespace?: string) => {
-    if (typeof message === 'string') {
-        console.log(`[${getDate()}] [${namespace || DEFAULT_NAMESPACE}] [ERROR] ${message}`);
-    } else {
-        console.log(`[${getDate()}] [${namespace || DEFAULT_NAMESPACE}] [ERROR]`, message);
-    }
-};
-
-const getDate = () => {
-    return new Date().toISOString();
-};
-
-const logging = { info, warn, error };
-
-export default logging;
+export default routes;
