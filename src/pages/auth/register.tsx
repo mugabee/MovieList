b95e33d739d3ed0467bc7b072f5295f1b318136e
mugabe richard <mugabe.richard.37@gmail.com>
2021-12-07 11:32:34 +0200

    styling the home login page with responsiveness

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,105 +1,279 @@
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { Button, FormGroup, Input } from 'reactstrap';
-import AuthContainer from '../../components/AuthContainer';
-import ErrorText from '../../components/ErrorText';
-import { auth } from '../../config/firebase';
-import logging from '../../config/logging';
-import IPageProps from '../../interface/page';
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Button, FormGroup, Input } from "reactstrap";
+import AuthContainer from "../../components/AuthContainer";
+import ErrorText from "../../components/ErrorText";
+import { auth } from "../../config/firebase";
+import logging from "../../config/logging";
+import IPageProps from "../../interface/page";
 
-const RegisterPage: React.FunctionComponent<IPageProps> = props => {
-    const [registering, setRegistering] = useState<boolean>(false);
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [confirm, setConfirm] = useState<string>('');
-    const [error, setError] = useState<string>('');
+const RegisterPage: React.FunctionComponent<IPageProps> = (props) => {
+  const [registering, setRegistering] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirm, setConfirm] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const signUpWithEmailAndPassword = () => {
-        if (password !== confirm)
-        {
-            setError('Please make sure your passwords match.');
-            return;
-        }
-
-        if (error !== '') setError('');
-
-        setRegistering(true);
-
-        auth.createUserWithEmailAndPassword(email, password)
-        .then(result => {
-            logging.info(result);
-            history.push('/login');
-        })
-        .catch(error => {
-            logging.error(error);
-
-            if (error.code.includes('auth/weak-password'))
-            {
-                setError('Please enter a stronger password.');
-            }
-            else if (error.code.includes('auth/email-already-in-use'))
-            {
-                setError('Email already in use.');
-            }
-            else
-            {
-                setError('Unable to register.  Please try again later.')
-            }
-
-            setRegistering(false);
-        });
+  const signUpWithEmailAndPassword = () => {
+    if (password !== confirm) {
+      setError("Please make sure your passwords match.");
+      return;
     }
 
-    return (
-        <AuthContainer header="Register">
-            <FormGroup>
-                <Input 
+    if (error !== "") setError("");
+
+    setRegistering(true);
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        logging.info(result);
+        history.push("/login");
+      })
+      .catch((error) => {
+        logging.error(error);
+
+        if (error.code.includes("auth/weak-password")) {
+          setError("Please enter a stronger password.");
+        } else if (error.code.includes("auth/email-already-in-use")) {
+          setError("Email already in use.");
+        } else {
+          setError("Unable to register.  Please try again later.");
+        }
+
+        setRegistering(false);
+      });
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <AuthContainer header="Register">
+        <div
+          className="
+          flex flex-col
+          bg-white
+          shadow-md
+          px-4
+          sm:px-6
+          md:px-8
+          lg:px-10
+          py-8
+          rounded-3xl
+          w-50
+          max-w-md
+        "
+        >
+          <div className="font-medium self-center text-xl sm:text-3xl text-gray-800">
+            Join us Now
+          </div>
+          <div className="mt-4 self-center text-xl sm:text-sm text-gray-800">
+            Enter your credentials to get access account
+          </div>
+
+          <div className="mt-10">
+            <form action="#">
+              <div className="flex flex-col mb-5">
+                <label className="mb-1 text-xs tracking-wide text-gray-600">
+                  E-Mail Address:
+                </label>
+                <div className="relative">
+                  <div
+                    className="
+                    inline-flex
+                    items-center
+                    justify-center
+                    absolute
+                    left-0
+                    top-0
+                    h-full
+                    w-10
+                    text-gray-400
+                  "
+                  >
+                    <i className="fas fa-at text-blue-500"></i>
+                  </div>
+
+                  <input
                     type="email"
                     name="email"
                     id="email"
-                    placeholder="Email Address"
-                    onChange={event => setEmail(event.target.value)}
+                    onChange={(event) => setEmail(event.target.value)}
                     value={email}
-                />
-            </FormGroup>
-            <FormGroup>
-                <Input 
+                    className="
+                    text-sm
+                    placeholder-gray-500
+                    pl-10
+                    pr-4
+                    rounded-2xl
+                    border border-gray-400
+                    w-full
+                    py-2
+                    focus:outline-none focus:border-blue-400
+                  "
+                    placeholder="Enter your email"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col mb-6">
+                <label className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600">
+                  Password:
+                </label>
+                <div className="relative">
+                  <div
+                    className="
+                    inline-flex
+                    items-center
+                    justify-center
+                    absolute
+                    left-0
+                    top-0
+                    h-full
+                    w-10
+                    text-gray-400
+                  "
+                  >
+                    <span>
+                      <i className="fas fa-lock text-blue-500"></i>
+                    </span>
+                  </div>
+
+                  <input
                     autoComplete="new-password"
                     type="password"
                     name="password"
                     id="password"
-                    placeholder="Enter Password"
-                    onChange={event => setPassword(event.target.value)}
+                    placeholder="Enter your password"
+                    onChange={(event) => setPassword(event.target.value)}
                     value={password}
-                />
-            </FormGroup>
-            <FormGroup>
-                <Input 
+                    className="
+                    text-sm
+                    placeholder-gray-500
+                    pl-10
+                    pr-4
+                    rounded-2xl
+                    border border-gray-400
+                    w-full
+                    py-2
+                    focus:outline-none focus:border-blue-400
+                  "
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col mb-6">
+                <label className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600">
+                  Comfirm Password:
+                </label>
+                <div className="relative">
+                  <div
+                    className="
+                    inline-flex
+                    items-center
+                    justify-center
+                    absolute
+                    left-0
+                    top-0
+                    h-full
+                    w-10
+                    text-gray-400
+                  "
+                  >
+                    <span>
+                      <i className="fas fa-lock text-blue-500"></i>
+                    </span>
+                  </div>
+
+                  <input
                     autoComplete="new-password"
                     type="password"
                     name="confirm"
                     id="confirm"
-                    placeholder="Confirm Password"
-                    onChange={event => setConfirm(event.target.value)}
+                    placeholder="Comfirm Password"
+                    onChange={(event) => setConfirm(event.target.value)}
                     value={confirm}
-                />
-            </FormGroup>
-            <Button
-                disabled={registering}
-                color="success"
-                block
-                onClick={() => signUpWithEmailAndPassword()}
+                    className="
+                    text-sm
+                    placeholder-gray-500
+                    pl-10
+                    pr-4
+                    rounded-2xl
+                    border border-gray-400
+                    w-full
+                    py-2
+                    focus:outline-none focus:border-blue-400
+                  "
+                  />
+                </div>
+              </div>
+
+              <div className="flex w-full">
+                <button
+                  disabled={registering}
+                  color="success"
+                  onClick={() => signUpWithEmailAndPassword()}
+                  type="submit"
+                  className="
+                  flex
+                  mt-2
+                  items-center
+                  justify-center
+                  focus:outline-none
+                  text-white text-sm
+                  sm:text-base
+                  bg-blue-500
+                  hover:bg-blue-600
+                  rounded-2xl
+                  py-2
+                  w-full
+                  transition
+                  duration-150
+                  ease-in
+                "
+                >
+                  <span className="mr-2 uppercase">Sign Up</span>
+                  <span>
+                    <svg
+                      className="h-6 w-6"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                  </span>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="flex justify-center items-center mt-6">
+          <small>
+            <p
+              className="inline-flex
+            items-center
+            text-gray-700
+            font-medium
+            text-xs text-center"
             >
-                Sign Up
-            </Button>
-            <small>
-                <p className='m-1 text-center'>Already have an account? <Link to="/login">Login.</Link></p>
-            </small>
-            <ErrorText error={error} />
-        </AuthContainer>
-    );
-}
+              <span className="ml-2">
+                Already have an account?{" "}
+                <Link to="/login">
+                  <span className="text-xs ml-2 text-blue-500 font-semibold">
+                    Login.{" "}
+                  </span>
+                </Link>
+              </span>
+            </p>
+          </small>
+        </div>
+        <ErrorText error={error} />
+      </AuthContainer>
+    </div>
+  );
+};
 
 export default RegisterPage;
