b5f6ba23ed38acbcbdb6281ef2979487932f4238
mugabe richard <mugabe.richard.37@gmail.com>
2021-12-07 22:16:06 +0200

    styling the home login page with responsiveness

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -17,6 +17,10 @@ const HomePage: React.FunctionComponent<IPageProps> = (props) => {
   const handleAddToList = (movie: any) => {
     addToList(movie);
   };
+  const [dropMenu, setDropMenu] = useState(false);
+  const showMenu = () => {
+    setDropMenu(!dropMenu);
+  };
 
   console.log(watchList);
 
@@ -62,6 +66,22 @@ const HomePage: React.FunctionComponent<IPageProps> = (props) => {
                 </button>
             </div>
         </div>
+
+        <div className="relative mx-auto text-gray-600 lg:block hidden">
+                <input
+                    className="border-2 border-gray-300 bg-white h-10 pl-2 pr-48 rounded-lg text-sm focus:outline-none"
+                    type="search" name="search" placeholder="Search"/>
+                <button type="submit" className="absolute right-0 top-0 mt-3 mr-2">
+                    <svg className="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                         version="1.1" id="Capa_1" x="0px" y="0px"
+                         viewBox="0 0 56.966 56.966" 
+                       
+                         width="512px" height="512px">
+                <path
+                    d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"/>
+              </svg>
+                </button>
+            </div>
     
         <div className="menu w-full lg:block flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
             <div className="text-md font-bold text-blue-700 lg:flex-grow">
@@ -78,22 +98,35 @@ const HomePage: React.FunctionComponent<IPageProps> = (props) => {
                     Menu 3
                 </a>
             </div>
-           
-            <div className="relative mx-auto text-gray-600 lg:block hidden">
-                <input
-                    className="border-2 border-gray-300 bg-white h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-none"
-                    type="search" name="search" placeholder="Search"/>
-                <button type="submit" className="absolute right-0 top-0 mt-3 mr-2">
-                    <svg className="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
-                         version="1.1" id="Capa_1" x="0px" y="0px"
-                         viewBox="0 0 56.966 56.966" 
-                       
-                         width="512px" height="512px">
-                <path
-                    d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"/>
-              </svg>
-                </button>
+            <div>
+            {genres && (
+            <select name="genre" onChange={() => handleChange}>
+              <option>select movies type</option>
+              {genres.map((genre) => (
+                <option value={genre.id}>{genre.name}</option>
+              ))}
+              <option>kids movies</option>
+            </select>
+          )}
             </div>
+            <div>
+            <span onClick={showMenu}>{auth.currentUser?.displayName}</span>
+            <div
+              className={`${
+                dropMenu ? "" : "hidden"
+              }  absolute w-full  rounded-b border-t-0 z-10`}
+            >
+                <div>
+                <button><Link to="/change">Change Password</Link> </button>
+                    <button><Link to="/logout">Logut</Link> </button>
+                </div>
+                </div>
+                
+            </div>
+            
+
+           
+            
             <div className="flex ">
                 <a href="#"
                    className="block text-md px-4 py-2 rounded text-blue-700 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">Sign
